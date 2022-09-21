8a2336eee21beef52a294cd8b4b0b7b63049b748
damblatt <dam.blatt@gmail.com>
2022-09-21 15:12:04 +0200

    Menu: Anzahl Mannschaften auswählen

--- gruempeliturnier/Program.cs
+++ gruempeliturnier/Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // variables
+            int amountOfTeams = 0;
+
+            Console.WriteLine("Hello, this program will help you managing your Gruempeliturnier.");
+            Console.WriteLine("First of all, tell us how many teams are going to participate?");
+            while (amountOfTeams == 0 || amountOfTeams <= 0)
+            {
+                Console.Write("Enter a positive number: ");
+                amountOfTeams = Convert.ToInt32(Console.ReadLine());
+                if (amountOfTeams > 0) { Console.WriteLine($"\n{amountOfTeams} Teams are going to participate."); }
+                else { Console.WriteLine($"There can't be {amountOfTeams} teams"); }
+            }
         }
     }
 }
-
-// Branch Damian xdasdf
-// zeile code
